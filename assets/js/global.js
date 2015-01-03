f94795ce2d036dd2cb239eeb87e2509e5c912355
Benjamin Parry <benjamin@parry.is>
2015-01-03 22:26:20 +0000

    Add gradient bg, simplify markup, change interation, replace missing images.

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	$('.spectrum--loading .spectrum__colour__loader').animate({
 		"height": "100%"},
 		3000, function() {
-			$('.spectrum__colour__image').animate({"opacity": "1.0"}, 1200);
+			$('.spectrum__colour').addClass("spectrum__colour--loaded");
+			$('.spectrum__colour__loader').animate({"opacity": "0"}, 1200);
 	});
 });
